528d0c77bcbc0c4cf8fb43119228673e6bedc9c9
Mikey Austin <mikey@mikeyaustin.com>
2017-03-30 00:00:40 +0200

    remove committed generated test prog again

--- check/test_prog_runner.sh
+++
@@ -1,6 +0,0 @@
-VALGRIND="/usr/bin/valgrind"
-if [ -x $VALGRIND ]; then
-    $VALGRIND -q --trace-children=yes --track-origins=yes --leak-check=full --error-exitcode=1 --tool=memcheck $@
-else
-    $@
-fi
