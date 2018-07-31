cb8c60c5d35b6e19101b875775e28d256cddb1b0
Raphael Sousa Santos <contact@raphaelss.com>
2018-07-31 22:41:46 +0200

    Add stu-mode.el to highlight the proper keywords and regexes

---
+++ stu-mode.el
@@ -0,0 +1,16 @@
+;;; For the keyword list, add it to the following command, generate it and then
+;;; replace the line in stu-font-lock-keywords while keeping the right \\_>
+;;; (regexp-opt '("(λ" "(def" "(lambda" "(if" "(quote") 'symbols)
+
+(defvar stu-font-lock-keywords
+  '(("#/.*/i?" . font-lock-string-face)
+    ("\\((\\(?:def\\|if\\|lambda\\|quote\\|λ\\)\\)\\_>" . font-lock-keyword-face)))
+
+(define-derived-mode stu-mode lisp-mode "Stu"
+  "Major mode for editing stu code"
+  (set (make-local-variable 'font-lock-defaults) '(stu-font-lock-keywords)))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.stu\\'" . stu-mode))
+
+(provide 'stu-mode)
